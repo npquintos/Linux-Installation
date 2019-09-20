a414711a7efa2b4ef0c22b05201ac9e9d8342494
Noel Quintos <npquintos@gmail.com>
2019-09-20 13:08:26 -0600

    Update link_dot_files.sh

--- link_dot_files.sh
+++ link_dot_files.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-skel_location=/etc/skel/
-for user_dir in $(grep home /etc/passwd | awk -F':' '{print $6}')
-do
-    echo ""
-    echo "linking dot files for ${user_dir}"
-    for dotfile in $(ls -A ${skel_location})
-    do
-        echo "    creating soft link for ${skel_location}${dotfile} --> ${user_dir}/${dotfile}"
-        # ln -s ${skel_location}${dotfile} ${user_dir}/${dotfile}
-    done
+myhome="/home/$(whoami)"
+mysource="/tmp/dotfiles"
+for i in $(ls ${mysource}/__*); do
+    j=${i##*/}
+    k=${j/__/\.}
+    cp $i /etc/skel/$k
+    ln -s /etc/skel/$k ${myhome}/$k
 done
