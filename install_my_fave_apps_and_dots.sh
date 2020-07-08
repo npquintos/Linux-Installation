d04f9d3fa1d5f3801169ae6065b91ceb6b44c51d
Noel Quintos <npquintos@gmail.com>
2020-07-07 22:15:06 -0600

    Update install_my_fave_apps_and_dots.sh

--- install_my_fave_apps_and_dots.sh
+++ install_my_fave_apps_and_dots.sh
@@ -3,9 +3,5 @@ cd /tmp
 git clone https://github.com/npquintos/dotfiles.git
 cd /tmp/Linux-Installation
 bash link_dot_files.sh
-cd /tmp
-git clone https://git.suckless.org/st
-cd st
-make clean install
-cd /tmp
+
 
