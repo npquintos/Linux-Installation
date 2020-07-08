685f10fc67f6ec1a774aa4280e7330f6eaa4dbb1
Noel Quintos <npquintos@gmail.com>
2020-07-08 13:57:06 -0600

    Add files via upload

---
+++ install_my_fave_apps_and_dots.sh
@@ -0,0 +1,7 @@
+bash install_my_favourite_apps.sh
+cd /tmp
+git clone https://github.com/npquintos/dotfiles.git
+cd /tmp/Linux-Installation
+bash link_dot_files.sh
+cp /tmp/dotfiles/make_link_to_dotfiles.sh /usr/local/bin/.
+chmod a+x /usr/local/bin/make_link_to_dotfiles.sh
---
+++ install_my_favourite_apps.sh
@@ -0,0 +1,26 @@
+# !/bin/bash
+
+function install_my_favorite() {
+    IFS=$'\n'
+    echo "about to install packages contained in $1"
+    apt-get update
+    for APP in $(sed 's/ *#.*$//' $1)
+    do
+        # echo "APP Line: ${APP}"
+        # INFO[0] = App name; INFO[1] = App ppa
+        IFS='|'; read -a INFO <<< "${APP}"
+        if [ "${INFO[1]}" ]; then
+            eval "add-apt-repository ${INFO[1]}"
+            apt-get update
+        fi
+        # echo "app name: ${INFO[0]}     PPA: >${INFO[1]}<"
+        if which ${INFO[0]} > /dev/null 2>&1; then
+            echo "     ${INFO[0]} already installed !"
+        else
+            echo "     installing ${INFO[0]}"
+            eval "apt-get install ${INFO[0]}"
+        fi
+    done
+}
+
+install_my_favorite list_of_apps.txt
