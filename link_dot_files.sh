ba383f37cd68d018ce8f53ef24eeb9ddd4957d6f
Noel Quintos <npquintos@gmail.com>
2020-07-08 13:56:11 -0600

    Delete link_dot_files.sh

--- link_dot_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-myhome="/home/$(whoami)"
-mysource="/tmp/dotfiles"
-for i in $(ls ${mysource}/__*); do
-    j=${i##*/}
-    k=${j/__/\.}
-    cp $i /etc/skel/$k
-    ln -s /etc/skel/$k ${myhome}/$k
-done
