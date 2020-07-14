6ea9be9cfb3a8a98557ef4c9af961254e24d7629
Noel Quintos <npquintos@gmail.com>
2020-07-13 21:37:25 -0600

    Update link_dot_files.sh

--- link_dot_files.sh
+++ link_dot_files.sh
@@ -8,13 +8,13 @@ for i in $(ls ${mysource}/__*); do
     echo "cp $i /etc/skel/$k"
     cp -f $i /etc/skel/$k
     echo "ln -s /etc/skel/$k ${myhome}/$k"
-    ln -s /etc/skel/$k ${myhome}/$k
+    ln -nsf /etc/skel/$k ${myhome}/$k
 done
 cp -f ${mysource}/init.vim /etc/skel/init.vim
 mkdir -p ${myhome}/.config/nvim
-ln -s /etc/skel/init.vim ${myhome}/.config/nvim/.
+ln -nsf /etc/skel/init.vim ${myhome}/.config/nvim/.
 mkdir -p /etc/skel/color
 cp -f ${mysource}/flattened_darkq.vim /etc/skel/color/.
 cp -f ${mysource}/flattened_light.vim /etc/skel/color/.
-ln -s /etc/skel/color/flattened_darkq.vim ${myhome}/.vim/colors/.
-ln -s /etc/skel/color/flattened_light.vim ${myhome}/.vim/colors/.
+ln -nsf /etc/skel/color/flattened_darkq.vim ${myhome}/.vim/colors/.
+ln -nsf /etc/skel/color/flattened_light.vim ${myhome}/.vim/colors/.
