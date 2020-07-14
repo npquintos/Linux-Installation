e4430776785dc445c8ccd51582c9ec17cea08f8e
Noel Quintos <npquintos@gmail.com>
2020-07-13 21:30:58 -0600

    Update link_dot_files.sh

--- link_dot_files.sh
+++ link_dot_files.sh
@@ -5,7 +5,9 @@ mkdir -p /etc/skel
 for i in $(ls ${mysource}/__*); do
     j=${i##*/}
     k=${j/__/\.}
+    echo "cp $i /etc/skel/$k"
     cp -f $i /etc/skel/$k
+    echo "ln -s /etc/skel/$k ${myhome)/$k"
     ln -s /etc/skel/$k ${myhome}/$k
 done
 cp -f ${mysource}/init.vim /etc/skel/init.vim
