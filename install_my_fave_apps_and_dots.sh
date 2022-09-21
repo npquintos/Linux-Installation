b2dbd448306f64737a1715fa70252ce9fafdad96
Noel Quintos <npquintos@gmail.com>
2022-09-20 21:31:52 -0600

    Update install_my_fave_apps_and_dots.sh

--- install_my_fave_apps_and_dots.sh
+++ install_my_fave_apps_and_dots.sh
@@ -5,3 +5,5 @@ cd /tmp/Linux-Installation
 bash ./link_dot_files.sh
 cp /tmp/dotfiles/make_link_to_dotfiles.sh /usr/local/bin/.
 chmod a+x /usr/local/bin/make_link_to_dotfiles.sh
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/install.sh -O - | bash
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
