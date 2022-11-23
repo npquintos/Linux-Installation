655e870654d69877acf75d57e1e269a7adddf295
Noel Quintos <npquintos@gmail.com>
2022-11-23 09:34:10 -0700

    Create miscellaneous.sh

---
+++ miscellaneous.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash 
+# Download Nerd Fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FantasqueSansMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Meslo.zip
+# Install PowerLevel10 prompt
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+# Install AstroNvim
